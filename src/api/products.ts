732c448aa6d59fae5a8d6f07303196bac3be9b88
eduardosm123 <magalhaeseduardo91@gmail.com>
2025-04-15 21:38:19 -0300

    removing console.log which shows the route

--- src/api/products.ts
+++ src/api/products.ts
@@ -5,7 +5,7 @@ const accessKey = process.env.NEXT_PUBLIC_API_KEY;
 
 export const getProduct = async (page: number, filter: string, typeFilter: string) => {
   try {
-    console.log(`/api-proxy/products?${typeFilter}=${filter}&limit=5&offset=${page}`)
+    
     const response = await axios.get(
       `/api-proxy/products?${typeFilter}=${filter}&limit=5&offset=${page}`,
       {
